ff94bf87349e043a61ca328f157d42d827a36fb2
Alex Harnett <harnettalex@gmail.com>
2025-06-09 19:33:55 -0700

    useReducer instead of useState

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,52 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useReducer } from 'react'
 import ToggleableAddProductForm from './components/ToggleableAddProductForm'
 import Header from './components/Header'
 import ProductListing from './components/ProductListing'
 import { type CartItem, type NewProduct, type Product as ProductType } from "./types.ts"
 import { addProduct, getProducts, deleteProduct, updateProduct, addProductToCart, getCartItems, checkout } from './services/products.ts'
 
+type ProductAction =
+  | { type: "SET_PRODUCTS"; payload: ProductType[] }
+  | { type: "ADD_PRODUCT"; payload: ProductType }
+  | { type: "DELETE_PRODUCT"; payload: { id: string } }
+  | { type: "UPDATE_PRODUCT"; payload: ProductType };
+
+const productsReducer = (prevState: ProductType[], action: ProductAction) => {
+  switch (action.type) {
+    case "ADD_PRODUCT":
+      return [...prevState, action.payload];
+    
+    case "DELETE_PRODUCT":
+      return prevState.filter(product => product._id !== action.payload.id);
+    
+    case "UPDATE_PRODUCT":
+      return prevState.map(product =>
+        product._id === action.payload._id ? action.payload : product
+      );
+      
+    case "SET_PRODUCTS":
+      return action.payload;
+    default:
+      throw new Error('Incorrect type given to productsReducer.')
+  }
+}
+
+const cartReducer = (_prevState: CartItem[], payload: CartItem[]) => {
+  return payload
+}
+
 function App() {
-  const [products, setProducts] = useState<ProductType[]>([])
-  const [cartItems, setCartItems] = useState<CartItem[]>([])
+  // const [products, setProducts] = useState<ProductType[]>([])
+  const [products, productsDispatch] = useReducer(productsReducer, [])
+  // const [cartItems, setCartItems] = useState<CartItem[]>([])
+  const [cartItems, cartItemsDispatch] = useReducer(cartReducer, [])
+  
 
   useEffect(() => {
     const initializeProducts = async() => {
       try {
         const data = await getProducts()
-        setProducts(data)
+        productsDispatch({ type: "SET_PRODUCTS", payload: data })
       } catch (e) {
         console.log('Error: ', e)
       }
@@ -22,7 +55,7 @@ function App() {
     const initializeCart = async () => {
       try {
         const data = await getCartItems()
-        setCartItems(data)
+        cartItemsDispatch(data)
       } catch (e) {
         console.log('Error: ', e)
       }
@@ -36,7 +69,7 @@ function App() {
     try {
       const data = await addProduct(newProduct)
       console.log(data);
-      setProducts((prev) => prev.concat(data))
+      productsDispatch({type: "ADD_PRODUCT", payload: data})
       if (callback) {
         callback()
       }
@@ -48,7 +81,7 @@ function App() {
   const handleDeleteProduct = async (productId: string) => {
     try {
       await deleteProduct(productId)
-      setProducts(products.filter(product => product._id !== productId))
+      productsDispatch({ type: "DELETE_PRODUCT", payload: { id: productId } })
     } catch(e) {
       console.log('Error: ', e)
     }
@@ -57,13 +90,7 @@ function App() {
   const handleUpdateProduct = async (product: ProductType) => {
     try {
       const data = await updateProduct(product)
-      setProducts(products.map(product => {
-        if (product._id === data._id) {
-          return data
-        } else {
-          return product
-        }
-      }))
+      productsDispatch({ type: "UPDATE_PRODUCT", payload: data })
     } catch (e) {
       console.log('Error: ', e)
     }
@@ -72,24 +99,16 @@ function App() {
   const handleAddProductToCart = async (id: string) => {
     try {
       const data = await addProductToCart(id);
-      setProducts(products.map(product => {
-        if (product._id === data.product._id) {
-          return data.product
-        } else {
-          return product
-        }
-      }))
+      productsDispatch({ type: "UPDATE_PRODUCT", payload: data.product })
 
-    setCartItems(prevItems => {
-      const index = prevItems.findIndex(item => item._id === data.cartItem._id);
+      const index = cartItems.findIndex(item => item._id === data.cartItem._id);
       if (index === -1) {
-        return [...prevItems, data.cartItem];
+        cartItemsDispatch([...cartItems, data.cartItem]);
+      } else {
+        cartItemsDispatch(cartItems.map(item =>
+          item._id === data.cartItem._id ? data.cartItem : item
+        ))
       }
-
-      return prevItems.map(item =>
-        item._id === data.cartItem._id ? data.cartItem : item
-      );
-    });
 
     } catch (e) {
       console.log('Error: ',  e)
@@ -99,7 +118,7 @@ function App() {
   const handleCheckout = async () => {
     try {
       await checkout()
-      setCartItems([])
+      cartItemsDispatch([])
     } catch (e) {
       console.log('Error: ', e)
     }
